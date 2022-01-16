ee215ada86920a633b4b14c92848dad7548cc5a5
HUEN OH <huen.oh@ssense.com>
2022-01-16 15:20:02 -0500

    clean code

--- helpers.py
+++ helpers.py
@@ -1,20 +1,5 @@
 import random
 
-def test_print(name):
-    print('my name is ', name)
-    
-def init_asteroids():
-    asteroidy = []
-    asteroidx = []
-    xspeed = []
-    yspeed = []
-    for i in range(15):
-        asteroidy.append(random.randint(-200, 1200))
-        asteroidx.append(random.randint(-200,1000))
-        xspeed.append(random.randint(-3, 3))
-        yspeed.append(random.randint(-3, 3))
-    return asteroidx, asteroidy, xspeed, yspeed
-
 def is_collided(obj1, obj2):
     circles1 = obj1.get_collision_circles()
     circles2 = obj2.get_collision_circles()
